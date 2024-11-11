afab35d2fe7d1e1993535d6b6c86dcc5f7038dc6
Pete Miller <miller.pete@gmail.com>
2024-11-11 13:36:33 -0800

    SiteInfo::id -> SiteInfo::uuid for ios build, and some style review feedback

--- components/ai_chat/core/browser/ai_chat_service.h
+++ components/ai_chat/core/browser/ai_chat_service.h
@@ -189,7 +189,6 @@ class AIChatService : public KeyedService,
   void OnDataDeletedForDisabledStorage(bool success);
 
   bool IsAIChatHistoryEnabled();
-  base::SequencedTaskRunner* GetDBTaskRunner();
 
   raw_ptr<ModelService> model_service_;
   raw_ptr<PrefService> profile_prefs_;
@@ -205,8 +204,6 @@ class AIChatService : public KeyedService,
 
   // Storage for conversations
   base::SequenceBound<AIChatDatabase> ai_chat_db_;
-
-  scoped_refptr<base::SequencedTaskRunner> db_task_runner_;
 
   // All conversation metadata. Mainly just titles and uuids. Key is uuid
   ConversationMap conversations_;
